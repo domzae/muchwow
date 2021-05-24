0b8ef5211d985271481848efdd11536f5d2dad00
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-05-24 12:43:28 +0200

    implemented script, added exception handling

--- muchwow/lib.py
+++ muchwow/lib.py
@@ -2,7 +2,14 @@ from numpy import random
 import webbrowser
 
 def try_me():
-    inp = abs(int(input("Give me a number ;) : ")))
+    while True:
+        try:
+            inp = input("Give me a number ;) : ")
+            inp = abs(int(float(inp)))
+            break
+        except:
+            print('Not a number!')
+
     webbrowser.open(f'https://www.memedroid.com/memes/random/{inp}', new=2)
     
 if __name__ == "__main__":
